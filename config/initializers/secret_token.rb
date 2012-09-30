d460ae8a08b2c9d24159299f2671ca4212381145
Bruno Skribulja <skiczg@gmail.com>
2012-09-30 22:44:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sirius::Application.config.secret_token = '8367b170999649b4dc823d05bdf5ae53b948c8b203e1c5581ba15bc9d9e8a7c59ff4d49150af6d6b0410cf1d3db524f17c4b85f9ec40ab1a03276aa3055d9c4c'
